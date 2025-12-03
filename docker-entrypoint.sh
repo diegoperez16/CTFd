5b140095d9d7c6e2139b7a887a4cdfcc33d61751
diegoperez16 <diego.perez16@upr.edu>
2025-12-03 15:44:46 -0400

    no quotes in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,7 +30,7 @@ flask db upgrade
 
 # Start CTFd
 echo "Starting CTFd"
-exec gunicorn 'CTFd:create_app()' \
+exec gunicorn CTFd:create_app() \
     --bind "0.0.0.0:$PORT" \
     --workers $WORKERS \
     --worker-tmp-dir "$WORKER_TEMP_DIR" \
